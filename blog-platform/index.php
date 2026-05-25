54b2d99bb6ca625f4bf92162d3009e9ddfd45690
epe129 <eemelik985@gmail.com>
2026-05-25 17:34:08 +0300

    commit

--- blog-platform/index.php
+++ blog-platform/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION['user_id'])) {
-    header('Location: dashboard.php');
+    header('Location: ./frondend/dashboard.php');
 } else {
-    header('Location: login.php');
+    header('Location: ./frondend/login.php');
 }
 exit;
